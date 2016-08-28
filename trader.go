736b2fd11e3ebb4d306494ad9e6cfd59972bb204
Manuel HUEZ <manuel.huez@gmail.com>
2016-08-27 23:01:16 -0400

    Update pointer receiver

--- trader.go
+++ trader.go
@@ -36,7 +36,7 @@ func (t Trader) FindCurrency(code string) (*Currency, error) {
 // SetBaseCurrency sets the base currency for the Trader, from the given
 // Currency code. If the currency code was not found in the currencies of the
 // Trader, an error is returned
-func (t Trader) SetBaseCurrency(code string) error {
+func (t *Trader) SetBaseCurrency(code string) error {
 	c, err := t.FindCurrency(code)
 	if err != nil {
 		return err
